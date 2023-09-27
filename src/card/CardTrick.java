5ae9cc7fcbbe385f9c2e30f36fdf8c38ce5bf5f7
NsikakAbasi Ibangha <ibangha@sheridancollege.ca>
2023-09-26 22:30:02 -0400

    Putting this in the local yeaa

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -4,28 +4,65 @@
  */
 package card;
 
+import java.util.*;
+import java.lang.Math;
+
 /**
- * A class that fills a magic hand of 7 cards with random Card Objects
- * and then asks the user to pick a card and searches the array of cards
- * for the match to the user's card. To be used as starting code in ICE 1
- * @author srinivsi
+ * A class that fills a magic hand of 7 cards with random Card Objects and then
+ * asks the user to pick a card and searches the array of cards for the match to
+ * the user's card. To be used as starting code in ICE 1
+ *
+ * @author NsikakAbasi Ibangha
  */
 public class CardTrick {
-    
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
+        Random random = new Random();
+        int ranVal = random.nextInt(14);
+        int ranSui = random.nextInt(4);
+
+        for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(ranVal);//random number generator
+            c.setSuit(Card.SUITS[ranSui]);//Card.Suits
+            magicHand[i] = c;
+
         }
-        
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+
+        Scanner userin = new Scanner(System.in); //user input in the scanner 
+        System.out.println("Hello user pick a card any card, input the suit and value;");
+
+        String userSui = userin.nextLine(); // assigning a variable to the user input
+        int userVal = userin.nextInt();
+
+        Card userCard = new Card(); // creating a card for the user using their input
+        userCard.setSuit(userSui);
+        userCard.setValue(userVal);
+
+        boolean compSuit = false; // condition of the comparison of users card and the cards in magic hand
+        boolean compVal = false;
+
+        int check = 0;
+        // while ensures that each element in the array is checked
+        // the loop contains if statements with conditions that compare the suit and number values of the user's card and of the current card in the array
+        // once the condition is met the compSuit and compVal are made true
+        do {
+            if (userCard.getSuit().equals(magicHand[check].getSuit())) {
+                compSuit = true;
+            }
+
+            if (userCard.getValue() == magicHand[check].getValue()) {
+                compVal = true;
+            }
+            ++check;
+        } while (check < 7);
+
+        //final statement that makes the final check, if both 'comp' values are true than a message is sent accordingly 
+        if (compSuit == true && compVal == true) {
+            System.out.println("You have selected the right card");
+        } else {
+            System.out.println("You do not have the right card :=<");
+        }
     }
-    
 }
